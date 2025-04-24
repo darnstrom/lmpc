e68fd6295a084f4ab49eaf0e5b9fb72ee540856d
Daniel Arnström <daniel.arnstrom@liu.se>
2025-04-25 01:46:19 +0200

    Basic interface working

--- src/lmpc/lmpc.py
+++ src/lmpc/lmpc.py
@@ -15,7 +15,44 @@ LinearMPC = jl.LinearMPC
 class MPC:
     jl_mpc:AnyValue
     def __init__(self,F,G):
-        LinearMPC.MPC(F,G)
+        self.jl_mpc = LinearMPC.MPC(F,G)
 
     def compute_control(self,x,r=None, d=None, uprev=None):
-        return  ParametricDAQP.compute_control(self.jl_mpc,x, r = r, d=d, uprev=uprev)
+        return  LinearMPC.compute_control(self.jl_mpc, x, r = r, d=d, uprev=uprev)
+    
+    def setup(self):
+        LinearMPC.setup_b(self.jl_mpc)
+
+    def set_bounds(self, umin=np.zeros(0), umax=np.zeros(0)):
+        LinearMPC.set_bounds_b(self.jl_mpc, umin = umin, umax = umax)
+
+    def add_constraint(self,Ax = None, Au= None, 
+                        Ar = np.zeros((0,0)), Aw = np.zeros((0,0)), 
+                        Ad = np.zeros((0,0)), Aup = np.zeros((0,0)),
+                        ub = np.zeros(0), lb = np.zeros(0),
+                        ks = None, soft=False, binary=False, prio = 0):
+        ks = range(1,self.jl_mpc.Np-1) if ks is None else [k+1 for k in ks]
+        LinearMPC.add_constraint_b(self.jl_mpc, Ax=Ax, Au=Au, Ar=Ar, Ad=Ad, Aup=Aup, ub=ub, lb=lb,
+                                 ks=ks, soft=soft, binary=binary, prio = prio)
+
+    def set_output_bounds(self, ymin=np.zeros(0), ymax=np.zeros(0), ks =None, soft = True, binary=False, prio = 0):
+        ks = range(1,self.jl_mpc.Np-1) if ks is None else [k+1 for k in ks]
+        LinearMPC.set_output_bounds_b(self.jl_mpc,ymin=ymin,ymax=ymax, 
+                                    ks=ks, soft=soft, binary=binary, prio=prio)
+
+    def set_weights(self, Q=None, R=None ,Rr=None, S=None, rho=None, Qf=None):
+        LinearMPC.set_weights_b(self.jl_mpc, Q=Q, R=R, Rr=Rr,S=S, rho=rho, Qf=Qf)
+
+    def set_terminal_cost(self):
+        LinearMPC.set_temrinal_cost_b(self.jl_mpc)
+
+    def set_prestabilizing_feedback(self, K=None):
+        if K is not None:
+            LinearMPC.set_prestabilizing_feedback_b(self.jl_mpc,K)
+        else:
+            LinearMPC.set_prestabilizing_feedback_b(self.jl_mpc)
+
+    def move_block(self,move):
+        LinearMPC.move_block_b(self.jl_mpc,move)
+
+
